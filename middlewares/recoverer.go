3808f2e241da526a1731f18b50d8220ec1ee65f5
Thibaut Roskam <throskam@gmail.com>
2025-11-30 11:31:16 +0100

    feat(recoverer): use request logger (#5)

--- middlewares/recoverer.go
+++ middlewares/recoverer.go
@@ -4,16 +4,18 @@ import (
 	"log/slog"
 	"net/http"
 	"runtime/debug"
+
+	"github.com/throskam/ki"
 )
 
 // Recoverer returns a middleware that recovers from panics and returns an Internal Server Error.
-func Recoverer(logger *slog.Logger) func(http.Handler) http.Handler {
+func Recoverer() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
 				if err := recover(); err != nil {
 					stack := debug.Stack()
-					logger.Error("panic recovered",
+					ki.MustGetLogger(r.Context()).Error("panic recovered",
 						slog.Any("error", err),
 						slog.String("stack", string(stack)),
 					)
